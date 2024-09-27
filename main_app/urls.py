ce72b3490651d2125e3ef871e1b4a3ed99dcc938
BHerbert <madammaj418@gmail.com>
2024-09-27 15:14:59 -0400

    Complete Part 2

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('records/', views.records, name='records'),
+    path('vinyls/', views.vinyls, name='vinyls'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Vinyl:
+    def __init__(self, name, artist, genre, description):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.description = description
+     
+
+vinyls = [
+    Vinyl('In My Lifetime Vol.1', 'Jay-Z', 'Hip-Hop', 'One of the best albums ever!'),
+    Vinyl('Experience Hendrix', 'Jimi Hendrix', 'Psychedelic Rock', 'I Love Jimi!!'),
+    Vinyl('Views', 'Drake', 'Hip-Hop', 'Every Saturday Morning Type Of Vibe To Blast!!')
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Welcome To My Vinyl Shop</h1>')
+    return render(request, 'home.html')
 
 def records(request):
-    return HttpResponse('<h1>My Records</h1>')
+    return render(request, 'records.html')
+
+def vinyls(request):
+    return render(request, 'vinyls/vinyls.html', {'vinyls': vinyls})
